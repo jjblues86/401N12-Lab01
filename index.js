2aa40dcbfd306b57ee9fa6a98b1cbf35ea5f9336
jjblues86 <jeromejoof@yahoo.com>
2019-02-05 20:38:41 -0800

    greeting test

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const greet = require('./lib/greet');
+const http = require('http');
+
+const requestHandler = (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 200;
+    res.write(greet.sayHello().toString());
+    res.end();
+};
+
+const app = http.createServer(requestHandler);
+app.listen(process.env.PORT, () => console.log('server is up'));
