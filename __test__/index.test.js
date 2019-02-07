13c0a077dfb488f5f86d4fc667eaee1e1629719f
jjblues86 <jeromejoof@yahoo.com>
2019-02-06 23:10:34 -0800

    arithmetic module test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,22 +2,23 @@
 
 const greet = require('../lib/greet');
 const faker = require('faker');
+const arithmetic = require('../lib/arithmetic');
 
 describe('#lib/greet.js', () => {
     // Jerome - here we'll write all our tests
 
     test('These are all the tests', () => {
         // ARRANGE
-        // const randomNumber = 15;
+        const randomNumber = 15;
         const aBoolean = true;
         const aName = faker.name.firstName();
         const world = 'world';
 
         // ACT
-        // no act
+        // Jerome - no act
 
         // ASSERT
-        // expect(greet(randomNumber)).toBeNull();
+        expect(greet(randomNumber)).toBeNull();
         expect(greet.sayHello(aBoolean)).toBeNull();
         expect(greet.sayHello(aName)).toBe(`Hello ${aName}`);
         expect(greet.sayHello(world)).toBe('Hello world');
@@ -25,3 +26,30 @@ describe('#lib/greet.js', () => {
     });
 
 });
+
+    describe('#lib/arithmetic.js', () => {
+        // Jerome - here we'll write all our arithmetic tests
+
+        test('These are all the arithmetic module tests', () => {
+            // ARRANGE
+            const oneNum = faker.random.number();
+            const twoNum = faker.random.number();
+            const threeNum = faker.random.number();
+
+            // ACT
+            // Jerome - no act
+
+            // ASSERT
+            expect(arithmetic.add(oneNum,twoNum)).toEqual(oneNum + twoNum);
+            expect(arithmetic.subtract(oneNum,twoNum)).toEqual(oneNum - twoNum);
+            expect(arithmetic.multiply(oneNum,twoNum)).toEqual(oneNum * twoNum);
+            expect(arithmetic.divide(oneNum,twoNum)).toEqual(oneNum / twoNum);
+
+            expect(arithmetic.add([oneNum,twoNum,threeNum])).toEqual(oneNum + twoNum + threeNum);
+            expect(arithmetic.subtract([oneNum,twoNum,threeNum])).toEqual(oneNum - twoNum - threeNum);
+            expect(arithmetic.multiply([oneNum,twoNum,threeNum])).toEqual(oneNum * twoNum * threeNum);
+            expect(arithmetic.divide(oneNum, 0)).toBeNull();
+
+        });
+    });
+
