41550c3faf7a30ae6969552e2e7ecd105f96ff6f
jjblues86 <jeromejoof@yahoo.com>
2019-02-06 19:58:44 -0800

    trying to get the tests to work

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -3,49 +3,25 @@
 const greet = require('../lib/greet');
 const faker = require('faker');
 
-describe('#greet.test.js', () => {
+describe('#lib/greet.js', () => {
     // Jerome - here we'll write all our tests
 
     test('You say hello', () => {
         // ARRANGE
         const randomNumber = 15;
-        const aNullValue = null;
-        const fifty = 50;
+        const aBoolean = true;
+        const aName = faker.name.firstName();
+        const world = 'world';
 
         // ACT
         // no act
 
         // ASSERT
-        expect(randomNumber).toEqual(15);
-        expect(fifty).toBeLessThan(100);
-        expect(fifty).toBeGreaterThan(10);
-        expect(aNullValue).toBeNull();
-        const firstArgument = faker.random.number();
-        const secondArgument = faker.random.number();
-        expect(myModule.myFunction(firstArgument, secondArgument).toEqual(firstArgument + secondArgument));
+        expect(greet(randomNumber)).toBeNull();
+        expect(greet(aBoolean)).toBeNull();
+        expect(greet(aName)).toEqual(`hello ${aName}`);
+        expect(greet(world)).toEqual('hello world');
 
-    });
-
-    test('Showcasing how to add another test ', () => {
-        //ARRANGE
-        const aTruthyValue = 'hi';
-        const aFalsyValue = 0;
-
-        //ACT
-
-        //ASSERT
-        expect(aTruthyValue).toBeTruthy();
-        expect(aFalsyValue).toBeFalsy();
-        expect(aTruthyValue).not.toBeFalsy();
-    });
-
-    test('greet.sayHi should say hi and ask about Jabin', () => {
-        //ARRANGE
-        //ACT
-        const returnValue = greet.sayHello('Jabin');
-
-        // ASSERT
-        expect(returnValue).toEqual('Hello Jabin. Have you met my dog khalesi?');
     });
 
 });
--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 'use strict';
 
 const greet = require('./lib/greet');
-const http = require('http');
 
-const requestHandler = (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    res.write(greet.sayHello().toString());
-    res.end();
-};
+console.log(greet('Jabin'));
 
-const app = http.createServer(requestHandler);
-app.listen(process.env.PORT, () => {
-    return console.log('server is up');
-});
+
